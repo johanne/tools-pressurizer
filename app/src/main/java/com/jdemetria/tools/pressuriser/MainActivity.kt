5bb3247b5e75a2fb61cd70e6c60689a1c6a1ba4e
johanne <jod.demetria@gmail.com>
2021-03-01 00:49:12 +0100

    Initial setup of project.

---
+++ app/src/main/java/com/jdemetria/tools/pressuriser/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.jdemetria.tools.pressuriser
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Example of a call to a native method
+        findViewById<TextView>(R.id.sample_text).text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+}
